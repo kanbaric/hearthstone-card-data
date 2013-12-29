f669db2e7676408a1bb0eea45e0aab236de8cf38
kanbaric <kanbaric@gmail.com>
2013-12-28 16:45:57 -0800

    Updates to card data mapper. Removed references to Curse’s site, since we’re not scraping any data from there. Updated readme. Refreshed cards.json.

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Hearthstone Card Data
 
 A usable and accurate hearthstone data source.
 
-* cards.json - An up to date list of card descriptions, scraped from ZAM and Curse sites.
+* cards.json - An up to date list of card descriptions, currently scraped from ZAM's hearthhead site.
 * utilities.js - Some JS utitlies used for processing card data.
 
 =====================
 
-This is a work in progress.  `cards.json` Should currently be useable, but I intend to add more metadata to the list such as Mechanic tags (Card Draw, Summon, etc), and maybe Flavor Text, Artist, etc.
+This is a work in progress.  `cards.json` should currently be useable, but I intend to add more metadata to the list such as Mechanic tags (Card Draw, Summon, etc), and maybe Flavor Text, Artist name, etc.
--- utilities.js
+++ utilities.js
@@ -1,3 +1,9 @@
+// To use this I currently use chrome dev tools to save and run these snippets, then
+// at a specific breakpoint while browsing hearthhead's card listing, I run:
+// `console.save(getZamHSData(this.data), 'cards.json');`
+//
+// This may change if hearthhead changes their js data structures.
+
 // console.save snippet
 // Originally from https://github.com/bgrins/devtools-snippets/blob/master/snippets/console-save/console-save.js
 // MIT Licensed
@@ -32,9 +38,24 @@
 // Authored by kanbaric
 // MIT Licensed
 (function(window) {
+
+    // Return all HS data
+    function getZamHSData (cardArray) {
+        return {
+            _comment: "//Retrieved on " + new Date().toString(),
+            cards: mapZamHSCards(cardArray),
+            labels: {
+                classes: getClasses(),
+                races: getRaces(),
+                types: getTypes(),
+                sets: getSets(),
+                quality: getQualities()
+            }
+        };
+    }
     
     // Prepare card data for json.stringify, stripping any dom references and processing ids.
-    window.parseZamHSCards = function (cardArray) {
+    function mapZamHSCards (cardArray) {
         var hsCards = [];
         
         if (cardArray && cardArray.length && cardArray.length > 0) {
@@ -46,15 +67,15 @@
                         cost: cardArray[i].cost,
                         attack: cardArray[i].attack,
                         health: cardArray[i].health,
-                        race: cardArray[i].race ? cardArray[i].race : null,
+                        race: cardArray[i].race ? cardArray[i].race : 0,
                         classs: cardArray[i].classs ? cardArray[i].classs : 0,
                         collectible: cardArray[i].collectible,
                         elite: cardArray[i].elite,
                         icon: cardArray[i].icon,
                         image: cardArray[i].image,
                         quality: cardArray[i].quality,
-                        set: cardArray[i].set,
-                        type: cardArray[i].type,
+                        set: cardArray[i].set ? cardArray[i].set : 0,
+                        type: cardArray[i].type ? cardArray[i].type : 0,
                         zamid: cardArray[i].id,
                         zampopularity: cardArray[i].popularity,
                     }
@@ -62,22 +83,31 @@
             }
         }
         return hsCards;
-    };
+    }
     
-    // Todo: Produce key-value pairs for various data types
-    //window.getClasses = function () {}
-    //window.getRaces = function () {}
-    //window.getTypes = function () {}
-    //window.getSets = function () {}
-    //window.getQualities = function () {}
-    //window.getMechanics = function () {}
+    // Produce id:label pairs for various data ids
+    function getClasses () {
+        return { 0: "Neutral", 1: "Warrior", 2: "Paladin", 3: "Hunter", 4: "Rogue", 5: "Priest", 6: "Death Knight", 7: "Shaman", 8: "Mage", 9: "Warlock", 10: "Monk", 11: "Druid" };
+    }
 
-    // Todo: return all HS data
-    //window.getZamHSData = function (cardArray) {
-    //    return {
-    //        cards: parseZamHSCards(cardArray),
-    //        classes: []
-    //    };
-    //}
+    function getRaces () {
+        return { 0: "None", 14: "Murloc", 15: "Demon", 20: "Beast", 21: "Totem", 23: "Pirate", 24: "Dragon" };
+    }
+
+    function getTypes () {
+        return { 0: "Unknown", 3: "Hero", 4: "Minion", 5: "Spell", 7: "Weapon", 10: "Hero Power" };
+    }
+
+    function getSets () {
+        return { 0: "Unknown", 2: "Basic", 3: "Expert", 4: "Reward", 5: "Missions", 11: "Promotion" };
+    }
+    
+    function getQualities () {
+        return { 0: "Free", 1: "Common", 3: "Rare", 4: "Epic", 5: "Legendary" };
+    }
+
+    //function getMechanics () {}
+
+    window.getZamHSData = getZamHSData;
 
 })(window);
